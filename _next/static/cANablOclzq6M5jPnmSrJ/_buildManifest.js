0b0d808724163b3e16caffaa8c1e87d11b79e980
serkayon <serkayon@gmail.com>
2026-03-27 16:33:56 +0530

    Updates

--- _next/static/cANablOclzq6M5jPnmSrJ/_buildManifest.js
+++ _next/static/cANablOclzq6M5jPnmSrJ/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/TYNATO//_next/:path+",
+        "destination": "/TYNATO/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
